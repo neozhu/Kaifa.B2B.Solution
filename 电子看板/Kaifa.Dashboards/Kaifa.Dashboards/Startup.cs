665307cfaa2480210a8f385de110fb54188a22d6
neozhu <new163@163.com>
2015-12-23 15:22:17 +0800

    添加电子看板

---
+++ 电子看板/Kaifa.Dashboards/Kaifa.Dashboards/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kaifa.Dashboards.Startup))]
+namespace Kaifa.Dashboards
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
