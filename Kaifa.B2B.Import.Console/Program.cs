4d002eda97bf39bc9fcf35dce0bb39e571c59de7
neozhu <new163@163.com>
2016-04-12 21:22:20 +0800

    fix

---
+++ Kaifa.B2B.Import.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kaifa.B2B.VendorAlloc;
+
+namespace Kaifa.B2B.Import.Console
+{
+    class Program
+
+    {
+     
+
+        static void Main(string[] args)
+        {
+            string connectionstring = System.Configuration.ConfigurationManager.AppSettings["connectionstring"];
+            string alldir = System.Configuration.ConfigurationManager.AppSettings["allocDir"];
+            string allbakdir = System.Configuration.ConfigurationManager.AppSettings["allocBakDir"];
+            string warehouse = System.Configuration.ConfigurationManager.AppSettings["warehouse"];
+
+             AllocTask allTask = new AllocTask(alldir, allbakdir, connectionstring, warehouse);
+            string caldir = System.Configuration.ConfigurationManager.AppSettings["calDir"];
+            string calbakdir = System.Configuration.ConfigurationManager.AppSettings["calBakDir"];
+
+            CalendarTask calTask = new CalendarTask(caldir, calbakdir, connectionstring, warehouse);
+            allTask.Start();
+            calTask.Start();
+            System.Console.ReadLine();
+
+        }
+    }
+}
